8806a246e30c09fdf1aab4821bcfdbcdb618086a
David GameDev <79913159+DavidOrtiz-GameDev@users.noreply.github.com>
2026-03-29 16:11:56 +0200

    Test update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidortiz-gamedev.github.io',
+  site: 'https://weddingtest.github.io',
   base: '/portfolio',
   vite: {
     // @ts-ignore
